c0e41a685d442f275408636d828e5da484b356c5
achuvr <m.akiyama@bipsee.co.jp>
2026-01-15 18:24:29 +0900

    CompleteHomework

--- Assets/Scripts/NewMonoBehaviourScript2.cs
+++ Assets/Scripts/NewMonoBehaviourScript2.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class NewMonoBehaviourScript2 : MonoBehaviour
 {
+    public PlayerEnemyHit pe;
+
+    private Image hpBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hpBar = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        hpBar.fillAmount = (float)pe.hp / 3f;
     }
 }
