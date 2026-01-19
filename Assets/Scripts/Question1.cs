6d58c1c52c94d6fb9c1ad30cd26805cc966e788d
DESKTOP-D5VSODH\MoriTomo <mori24tomo@gmail.com>
2026-01-19 20:41:25 +0900

    Add homework

--- Assets/Scripts/Question1.cs
+++ Assets/Scripts/Question1.cs
@@ -5,24 +5,31 @@ public class Question1 : MonoBehaviour
     public GameObject cube;
 
     private int a, b;
-
+    Color yellow = Color.yellow;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is created                                   
     void Start()
     {
+
+
+        
+
         for (a = 0; a < 9; a++)
         {
             for (b = 0; b < 9; b++)
             {
                     
-                Instantiate(cube, new Vector3(a*2, 0, b*2), Quaternion.identity);
-                   
-                    
+                cube = Instantiate(cube, new Vector3(a*2, 0, b*2), Quaternion.identity);
+                cube.GetComponent<MeshRenderer>().material.color = yellow;
+
             }
 
             
 
         }
+        
+       
+        
     }
 
     // Update is called once per frame
