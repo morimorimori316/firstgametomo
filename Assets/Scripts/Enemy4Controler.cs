060d10f3812a6639b8f83b7d08d042ef9c210d59
DESKTOP-D5VSODH\MoriTomo <mori24tomo@gmail.com>
2026-03-18 21:51:36 +0900

    Add homework

--- Assets/Scripts/Enemy4Controler.cs
+++ Assets/Scripts/Enemy4Controler.cs
@@ -5,22 +5,37 @@ public class Enemy4Controler : MonoBehaviour
     public bool isShooting = false;
 
     public GameObject cube;
+    public LineRenderer line;
+    float time = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        line.positionCount = 2;
+        //line = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
         if (isShooting)
         {
             
         }
         else
         {
+            time += Time.deltaTime;
             transform.LookAt(cube.transform);
+
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1,cube.transform.position);
+            if (time >= 5)
+            {
+                isShooting = true;
+                time = 0;
+            }
+
         }
     }
 }
