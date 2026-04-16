cd2b9479e0d22c0be446772ceafcd2ee1c9e8c0f
Masanori Akiyama <achuvr.business@gmail.com>
2026-04-16 18:13:28 +0900

    Add level text

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class PlayerManager : MonoBehaviour
 {
     public float level = 1;
     public float takeDownCount = 0;
     public float attack = 1;
     public Image experience;
-    
+    public TextMeshProUGUI levelText;
     float a = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        levelText.text = level.ToString();
     }
 
     public void TakeDown()
@@ -21,6 +22,8 @@ public class PlayerManager : MonoBehaviour
         if (takeDownCount == level)
         {
             level++;
+            levelText.text = level.ToString();
+            
             takeDownCount = 0;
             attack += 0.25f;
             GameObject.FindObjectOfType<Moviing>().speed += 2f;
